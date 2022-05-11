d6adc4c67d12d3c78d37abcfbdfbe35d95eba4b7
Vinicius Inzaurralde <87659334+vinixlr@users.noreply.github.com>
2022-05-11 13:56:49 -0300

    primeiro commit v0.001
    Primeiro commit código extrair dados do PDF

---
+++ README.md
@@ -0,0 +1,37 @@
+# renovacao-contratacao-lambda
+
+## PROJECT_REPO
+
+- [https://git....]
+
+## PROJECT_ID
+
+- nome_do_projeto
+
+## Descrição
+
+- descrição do projeto
+
+## LANG
+
+- Python
+
+
+## Runtime
+
+- Python 3.9
+
+
+## Integrações
+
+
+## Instalação
+
+
+## Variáveis de ambiente
+<table border='1'>
+
+</table
+
+## Tags
+
---
+++ main.py
@@ -0,0 +1,19 @@
+from extracao_dados import ExtrairDados
+from util import Util
+
+
+#lista de dados extraidos
+lista_dados_extraidos = ExtrairDados().extrair_dados()
+
+#lista de códigos extraidos
+lista_de_codigos = ExtrairDados().extrar_lista_codigos(lista_dados_extraidos)
+
+#lista de descricoes do PDF
+lista_descricoes = ExtrairDados().extrair_descricao(lista_dados_extraidos, lista_de_codigos)
+
+#extracao de SMV
+lista_de_smv = ExtrairDados().extrair_smv(lista_dados_extraidos, lista_de_codigos)
+
+
+
+Util().escrever_dados_planilha(lista_de_codigos, lista_descricoes, lista_de_smv)
